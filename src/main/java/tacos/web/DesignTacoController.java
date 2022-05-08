fbe094b1006a6661916b7ecdf3f711eccf0e5677
theEmperorofDaiViet <Khiet.To.05012001@gmail.com>
2022-05-08 23:48:30 +0700

    first commit

---
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,70 @@
+package tacos.web;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
+import tacos.Taco;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
+import tacos.Ingredient;
+import tacos.Ingredient.Type;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+	
+	private final IngredientRepository ingredientRepo;
+	private final TacoRepository tacoRepo;
+
+	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
+		this.ingredientRepo = ingredientRepo;
+		this.tacoRepo = tacoRepo;
+	}
+
+
+	@ModelAttribute
+	public void addIngredientsToModel(Model model) {
+		  List<Ingredient> ingredients = (List<Ingredient>) ingredientRepo.findAll();
+
+		  Type[] types = Ingredient.Type.values();
+			for (Type type : types) {
+				model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+			}
+		}
+
+		@GetMapping
+		public String showDesignForm(Model model) {
+			model.addAttribute("taco", new Taco());
+			return "design";
+		}
+
+		private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+			List<Ingredient> ingrList = new ArrayList<Ingredient>();
+			for (Ingredient ingredient: ingredients) {
+				if (ingredient.getType().equals(type)) ingrList.add(ingredient);
+			}
+			return ingrList;
+				
+		}
+		
+		@PostMapping
+		public String processDesign(@Valid Taco taco, Errors errors) {
+			if(errors.hasErrors()) {
+				return "design";
+			}
+			tacoRepo.save(taco);
+			log.info("Processing design: " + taco);
+			return "redirect:/orders/current";
+		}
+
+	}
